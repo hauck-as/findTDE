0e430f81dc991f20cdce3de90b3e13b626622105
hauck-as <ash5615@psu.edu>
2025-09-03 17:29:27 -0400

    Split LAMMPS into lammpsish and lammps for VASP files+LAMMPS calcs vs LAMMPS files+LAMMPS calcs

--- findtde/vasp_vel_write.py
+++ findtde/vasp_vel_write.py
@@ -1,66 +1,17 @@
 #!/usr/bin/env python3
 """Python module used to read lattice direction and KE text files and generate velocity vectors for VASP."""
 import os
+from pathlib import Path
 import subprocess
 import sys
 import re
-import math as m
-from pymatgen.io.vasp import Poscar, Potcar
-from pymatgen.io.lammps.inputs import LammpsInputFile
 import numpy as np
 
-atom_type, atom_num, lat_dir_pseudo, sim_prog = str(sys.argv[1]), int(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4])
-
-# set directory variables, using PWD as the base directory
-base_path = os.getcwd()
-bin_path, inp_path, perfect_path = os.path.relpath('bin', base_path), os.path.relpath('inp', base_path), os.path.relpath('perfect', base_path)
-if sim_prog == 'vasp':
-    tde_run_path = os.path.relpath(lat_dir_pseudo + '_' + atom_type + str(atom_num), base_path)
-    # set atomic weights from POTCAR
-    mass_lines = subprocess.run(['grep', 'POMASS', os.path.join(inp_path, 'POTCAR')], capture_output = True, text = True)
-    mass_lines_list = mass_lines.stdout.split('\n ')
-    atomic_masses = []
-    for i in range(len(mass_lines_list)):
-        atomic_masses.append(float(re.search(r'\d+.\d+', mass_lines_list[i]).group()))
-elif sim_prog == 'lammps':
-    tde_run_path = os.path.relpath(lat_dir_pseudo + '_' + atom_type + str(atom_num) + '_lmp', base_path)
-    # set atomic weights from LAMMPS input file
-    lmp_inp_f = LammpsInputFile.from_file(os.path.join(inp_path, 'input.tde'))
-    mass_list, group_list = lmp_inp_f.get_args('mass'), lmp_inp_f.get_args('group')
-    atom_mass_dict = {}
-    for i in group_list:
-        if 'type' in i:
-            atom_id_list = i.split(' type ')
-            for j in mass_list:
-                id_mass_list = j.split(' ')
-                if atom_id_list[1] == id_mass_list[0]:
-                    atom_mass_dict.update({atom_id_list[0]: float(id_mass_list[1])})
-    atomic_masses = list(atom_mass_dict.values())
-tde_run_outfile_path = os.path.relpath(os.path.join(tde_run_path, lat_dir_pseudo + '_' + atom_type + str(atom_num) + '_out.txt'), base_path)
-
-
-# utility functions
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
-
-
-def cart2sph(x, y, z):
-    dxy = np.sqrt(x**2 + y**2)
-    r = np.sqrt(dxy**2 + z**2)
-    theta = np.arctan2(y, x)
-    phi = np.arctan2(dxy, z)
-    theta, phi = np.rad2deg([theta, phi])
-    return r, theta % 360, phi
-
-
-def sph2cart(theta, phi, r=0.5):
-    theta, phi = np.deg2rad([theta, phi])
-    z = r * np.cos(phi)
-    rsinphi = r * np.sin(phi)
-    x = rsinphi * np.cos(theta)
-    y = rsinphi * np.sin(theta)
-    return x, y, z
+from ase.units import Angstrom, fs
+from ase.io.lammpsdata import read_lammps_data, write_lammps_data
+from pymatgen.io.vasp.inputs import Poscar, Potcar
+from pymatgen.io.lammps.inputs import LammpsInputFile
+from findtde.utils import unit_vector, cart2sph, sph2cart
 
 
 def vasp_inp_match_check(poscar_filepath, potcar_filepath):
@@ -91,7 +42,7 @@ def vasp_inp_match_check(poscar_filepath, potcar_filepath):
 
 
 # velocity vector calculation function
-def lat_vel_calc(M, E, dir):
+def lat_vel_calc(M, E, vdir):
     """
     Function to calculate velocity vector of an atom in the lattice.
     Given an atomic mass (u), energy (eV), and crystallographic direction,
@@ -103,17 +54,15 @@ def lat_vel_calc(M, E, dir):
     v_mag = np.sqrt(np.divide(2*E, M))
     v_mag *= 299792458*1e10*1e-15    # m/s * Angstrom/m * s/fs
     
-    v = np.zeros((v_mag.shape[0], dir.shape[0]))
+    v = np.zeros((v_mag.shape[0], vdir.shape[0]))
     
-    dir_mag = np.linalg.norm(dir)
-    dir_unit_vec = dir/dir_mag
+    dir_unit_vec = unit_vector(vdir)
     
     for i in range(v_mag.shape[0]):
         v[i, :] = np.multiply(v_mag[i], dir_unit_vec)
     return v
 
 
-# writing formatted velocity vector to POSCAR file
 def vel_to_POSCAR(vel_vec, atom_type, atom_no, filepath):
     """
     Function to write velocity vector of an atom in the lattice to the POSCAR file.
@@ -141,56 +90,135 @@ def vel_to_POSCAR(vel_vec, atom_type, atom_no, filepath):
     pos_file.write_file(filepath)
 
 
-# read lattice directions and kinetic energies from text files
-# finds lattice direction from pseudo passed from bash
-lat_dir_list_file = os.path.relpath('latt_dirs_to_calc.csv', base_path)
-with open(lat_dir_list_file) as ld_f:
-    for line in ld_f:
-        if line[:len(lat_dir_pseudo)] == lat_dir_pseudo:
-            lat_dir_list = re.split(r'[;,\s]\s*', line.strip('\n'))  # [u v w] or (rho, phi, theta)
-            break
+def vel_to_LAMMPS(vel_vec, atom_type, atom_no, filepath):
+    """
+    Function to write velocity vector of an atom in the lattice to the LAMMPS data file.
+    Given a velocity vector (metal units, Ang./ps), atom type, and the number of the atom
+    (from the data file). The velocity vector is a 1D numpy array, the atom type is a
+    string, and the atom number is an integer.
+    """
+    # read in LAMMPS data file with ASE
+    lmp_data = read_lammps_data(filepath, atom_style='atomic', units='metal', sort_by_id=True)
+    
+    # list of the element for each atom in the structure
+    atom_type_list = lmp_data.get_chemical_symbols()
+    # total number of atoms in the structure
+    total_ions = len(lmp_data)
+    
+    # get lattice vectors as a matrix
+    lmp_lat = lmp_data.get_cell()
+    
+    # set velocity list to array of zeros for all atoms
+    vel_arr = np.array([np.zeros(3) for i in range(total_ions)])
+    
+    # replace velocity line for desired atom with converted velocity vector
+    atom_type_list = list(map(lambda x: x.lower(), atom_type_list))
+    if atom_type.lower() in atom_type_list:
+        # atom_no-th line in the block corresponding to the correct atom_type - 1 (0-based indexing)
+        # for ASE, need to specify Ang./fs units for velocity vector (default time units are Ang.*sqrt(u/eV))
+        vel_arr[atom_no+atom_type_list.index(atom_type.lower())-1] = vel_vec*Angstrom/fs
+    
+    # set velocities in POSCAR to velocity array
+    lmp_data.set_velocities(vel_arr)
+    
+    write_lammps_data(filepath, lmp_data, masses=True, velocities=True, units='metal', atom_style='atomic')
+    
+    return lmp_data
 
-kinE_list_file = os.path.relpath(os.path.join(tde_run_path, 'KE_calcs_list.txt'), base_path)
-with open(kinE_list_file) as ke_f:
-    for line in ke_f:
-        pass
-    kinE_line = line  # eV
 
-kinE = np.array([float(kinE_line)])
-print('KE:', kinE, 'eV')
+if __name__ == '__main__':
+    atom_type, atom_num, lat_dir_pseudo, sim_prog = str(sys.argv[1]), int(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4])
 
-tde_kinE_path = os.path.relpath(os.path.join(tde_run_path, kinE_line.strip('\n') + 'eV'), base_path)
-pos_file = os.path.join(tde_kinE_path, 'POSCAR')
-pot_file = os.path.join(tde_kinE_path, 'POTCAR')
+    # set directory variables, using PWD as the base directory
+    base_path = Path.cwd()
+    bin_path, inp_path, perfect_path = base_path / 'bin', base_path / 'inp', base_path / 'perfect'
+    if sim_prog == 'vasp':
+        tde_run_path = base_path / lat_dir_pseudo + '_' + atom_type + str(atom_num)
+        # set atomic weights from POTCAR
+        mass_lines = subprocess.run(['grep', 'POMASS', inp_path / 'POTCAR'], capture_output = True, text = True)
+        mass_lines_list = mass_lines.stdout.split('\n ')
+        atomic_masses = []
+        for i in range(len(mass_lines_list)):
+            atomic_masses.append(float(re.search(r'\d+.\d+', mass_lines_list[i]).group()))
+    elif sim_prog == 'lammpsish' or sim_prog == 'lammps':
+        tde_run_path = base_path / lat_dir_pseudo + '_' + atom_type + str(atom_num) + '_lmp'
+        # set atomic weights from LAMMPS input file
+        lmp_inp_f = LammpsInputFile.from_file(inp_path / 'input.tde')
+        mass_list, group_list = lmp_inp_f.get_args('mass'), lmp_inp_f.get_args('group')
+        atom_mass_dict = {}
+        for i in group_list:
+            if 'type' in i:
+                atom_id_list = i.split(' type ')
+                for j in mass_list:
+                    id_mass_list = j.split(' ')
+                    if atom_id_list[1] == id_mass_list[0]:
+                        atom_mass_dict.update({atom_id_list[0]: float(id_mass_list[1])})
+        atomic_masses = list(atom_mass_dict.values())
+    tde_run_outfile_path = tde_run_path / lat_dir_pseudo + '_' + atom_type + str(atom_num) + '_out.txt'
 
-# check if POTCAR and POSCAR are congruent
-if vasp_inp_match_check(pos_file, pot_file) == False:
-    raise ValueError
+    # read lattice directions and kinetic energies from text files
+    # finds lattice direction from pseudo passed from bash
+    lat_dir_list_file = base_path / 'latt_dirs_to_calc.csv'
+    with open(lat_dir_list_file) as ld_f:
+        for line in ld_f:
+            if line[:len(lat_dir_pseudo)] == lat_dir_pseudo:
+                lat_dir_list = re.split(r'[;,\s]\s*', line.strip('\n'))  # [u v w] or (rho, phi, theta)
+                break
 
-# read in POSCAR file using pymatgen
-pos = Poscar.from_file(pos_file)
-lat_vecs = pos.structure.lattice.matrix
-pos_spec_list = pos.site_symbols
+    kinE_list_file = tde_run_path / 'KE_calcs_list.txt'
+    with open(kinE_list_file) as ke_f:
+        for line in ke_f:
+            pass
+        kinE_line = line  # eV
 
-if lat_dir_pseudo[-1] == 'L':
-    lat_dir = np.array(lat_dir_list[5:], dtype=int)
-    vel_dir = lat_dir@lat_vecs
-    print('lattice direction:', str(lat_dir))
-elif lat_dir_pseudo[-1] == 'S':
-    lat_dir = np.array(lat_dir_list[5:], dtype=float)
-    # rho can be any value since magnitude is scaled, choose 1 for simplicity
-    # first angle (phi) needs to be the angle given in the heatmap - 60 deg since heatmap adds 60 deg
-    cart_coords = np.array(sph2cart(float(lat_dir_list[5:][1]), float(lat_dir_list[5:][2]), r=1.))
-    vel_dir = cart_coords
-    print('spherical direction:', str(lat_dir))
+    kinE = np.array([float(kinE_line)])
+    print('KE:', kinE, 'eV')
 
-# calculate velocity vector and write to POSCAR file
-for i in range(len(pos_spec_list)):
-    if atom_type.lower() == pos_spec_list[i].lower():
+    tde_kinE_path = tde_run_path / kinE_line.strip('\n') + 'eV'
+
+    if sim_prog == 'vasp' or sim_prog == 'lammpsish':
+        pos_file = tde_kinE_path / 'POSCAR'
+        pot_file = tde_kinE_path / 'POTCAR'
+
+        # check if POTCAR and POSCAR are congruent
+        if vasp_inp_match_check(pos_file, pot_file) == False:
+            raise ValueError
+
+        # read in POSCAR file using pymatgen
+        pos = Poscar.from_file(pos_file)
+        lat_vecs = pos.structure.lattice.matrix
+        pos_spec_list = pos.site_symbols
+    
+    elif sim_prog == 'lammps':
+        data_file = tde_kinE_path / 'read_data.lmp'
+
+        # read in LAMMPS data file using ASE
+        lmp_data = read_lammps_data(data_file, atom_style='atomic', units='metal', sort_by_id=True)
+        lat_vecs = lmp_data.get_cell()
+
+    if lat_dir_pseudo[-1] == 'L':
+        lat_dir = np.array(lat_dir_list[5:], dtype=int)
+        vel_dir = lat_dir@lat_vecs
+        print('lattice direction:', str(lat_dir))
+    elif lat_dir_pseudo[-1] == 'S':
+        lat_dir = np.array(lat_dir_list[5:], dtype=float)
+        # rho can be any value since magnitude is scaled, choose 1 for simplicity
+        # first angle (phi) needs to be the angle given in the heatmap - 60 deg since heatmap adds 60 deg
+        cart_coords = np.array(sph2cart(float(lat_dir_list[5:][1]), float(lat_dir_list[5:][2]), r=1.))
+        vel_dir = cart_coords
+        print('spherical direction:', str(lat_dir))
+
+    # calculate velocity vector and write to file
+    if sim_prog == 'vasp' or sim_prog == 'lammpsish':
+        for i in range(len(pos_spec_list)):
+            if atom_type.lower() == pos_spec_list[i].lower():
+                vel_vecs = lat_vel_calc(atomic_masses[i], kinE, vel_dir)
+        vel_to_POSCAR(vel_vecs[0], atom_type, atom_num, pos_file)
+    elif sim_prog == 'lammps':
         vel_vecs = lat_vel_calc(atomic_masses[i], kinE, vel_dir)
-vel_to_POSCAR(vel_vecs[0], atom_type, atom_num, pos_file)
-print('velocity vectors:', vel_vecs)
+        vel_to_LAMMPS(vel_vecs[0], atom_type, atom_num, data_file)
+    print('velocity vectors:', vel_vecs)
 
-out_file = open(tde_run_outfile_path, 'a')
-out_file.write('direction:  '+str(lat_dir)+'\nvelocity vectors:  '+str(vel_vecs)+'\n')
-out_file.close()
+    out_file = open(tde_run_outfile_path, 'a')
+    out_file.write('direction:  '+str(lat_dir)+'\nvelocity vectors:  '+str(vel_vecs)+'\n')
+    out_file.close()
